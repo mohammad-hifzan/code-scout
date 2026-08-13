fb415b803cbc1e972a5ea5f41f456b4d0356d897
Mohammad Hifzan <misterj435@gmail.com>
2026-08-13 17:32:41 +0530

    test: reference categorizer

--- lib/reference_categorizer.rb
+++ lib/reference_categorizer.rb
@@ -5,7 +5,7 @@ class ReferenceCategorizer
   end
   
   def categorize(files)
-    {
+    result = {
       models: [],
       controllers: [],
       views: [],
@@ -16,41 +16,35 @@ class ReferenceCategorizer
       mailers: [],
       concerns: [],
       others: []
-    }.tap do |result|
-      files.each do |file|
-        case file
-        when %r{/app/models/}
-          result[:models] << file
+    }
 
-        when %r{/app/controllers/}
-          result[:controllers] << file
+    return result if files.nil?
 
-        when %r{/app/views/}
-          result[:views] << file
-
-        when %r{/app/helpers/}
-          result[:helpers] << file
-
-        when %r{/app/policies/}
-          result[:policies] << file
-
-        when %r{/app/services/}
-          result[:services] << file
-
-        when %r{/app/jobs/}
-          result[:jobs] << file
-
-        when %r{/app/mailers/}
-          result[:mailers] << file
-
-        when %r{/app/models/concerns/},
-             %r{/app/controllers/concerns/}
-          result[:concerns] << file
-
-        else
-          result[:others] << file
-        end
+    files.each do |file|
+      case file
+      when %r{/app/models/concerns/},
+           %r{/app/controllers/concerns/}
+        result[:concerns] << file
+      when %r{/app/models/}
+        result[:models] << file
+      when %r{/app/controllers/}
+        result[:controllers] << file
+      when %r{/app/views/}
+        result[:views] << file
+      when %r{/app/helpers/}
+        result[:helpers] << file
+      when %r{/app/policies/}
+        result[:policies] << file
+      when %r{/app/services/}
+        result[:services] << file
+      when %r{/app/jobs/}
+        result[:jobs] << file
+      when %r{/app/mailers/}
+        result[:mailers] << file
+      else
+        result[:others] << file
       end
     end
+    result
   end
 end
---
+++ spec/reference_categorizer_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+require_relative '../lib/reference_categorizer'
+
+RSpec.describe ReferenceCategorizer do
+  let(:categorizer) { described_class.new }
+
+  describe '#categorize' do
+    let(:empty_result) do
+      {
+        models: [], controllers: [], views: [], helpers: [], policies: [],
+        services: [], jobs: [], mailers: [], concerns: [], others: []
+      }
+    end
+
+    it 'handles nil input gracefully' do
+      expect(categorizer.categorize(nil)).to eq(empty_result)
+    end
+
+    it 'handles empty input' do
+      expect(categorizer.categorize([])).to eq(empty_result)
+    end
+
+    it 'categorizes various file types correctly' do
+      files = [
+        '/app/models/user.rb',
+        '/app/controllers/users_controller.rb',
+        '/app/views/users/index.html.erb',
+        '/app/policies/user_policy.rb',
+        '/app/services/user_service.rb',
+        '/app/jobs/user_job.rb',
+        '/app/mailers/user_mailer.rb',
+        '/app/helpers/user_helper.rb',
+        '/db/schema.rb' # other
+      ]
+
+      result = categorizer.categorize(files)
+
+      expect(result[:models]).to contain_exactly('/app/models/user.rb')
+      expect(result[:controllers]).to contain_exactly('/app/controllers/users_controller.rb')
+      expect(result[:views]).to contain_exactly('/app/views/users/index.html.erb')
+      expect(result[:policies]).to contain_exactly('/app/policies/user_policy.rb')
+      expect(result[:services]).to contain_exactly('/app/services/user_service.rb')
+      expect(result[:jobs]).to contain_exactly('/app/jobs/user_job.rb')
+      expect(result[:mailers]).to contain_exactly('/app/mailers/user_mailer.rb')
+      expect(result[:helpers]).to contain_exactly('/app/helpers/user_helper.rb')
+      expect(result[:others]).to contain_exactly('/db/schema.rb')
+    end
+
+    it 'categorizes model concerns correctly, not as models' do
+      files = ['/app/models/concerns/commentable.rb']
+      result = categorizer.categorize(files)
+
+      expect(result[:concerns]).to contain_exactly('/app/models/concerns/commentable.rb')
+      expect(result[:models]).to be_empty
+    end
+
+    it 'categorizes controller concerns correctly, not as controllers' do
+      files = ['/app/controllers/concerns/authenticatable.rb']
+      result = categorizer.categorize(files)
+
+      expect(result[:concerns]).to contain_exactly('/app/controllers/concerns/authenticatable.rb')
+      expect(result[:controllers]).to be_empty
+    end
+  end
+end
